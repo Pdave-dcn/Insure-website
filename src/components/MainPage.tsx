af10d9e2754c1d5f305bb05b6ca61e52541c115b
Pdave <ferdinandpdave@gmail.com>
2025-02-23 22:40:05 -0400

    Add advantages section to MainPage component

--- src/components/MainPage.tsx
+++ src/components/MainPage.tsx
@@ -1,8 +1,28 @@
 const MainPage = () => {
+  const advantages = [
+    {
+      image: "images/icon-snappy-process.svg",
+      name: "Snappy Process",
+      description:
+        "Our application process can be completed in minutes, not hours. Don't get stuck filling in tedious forms.",
+    },
+    {
+      image: "images/icon-affordable-prices.svg",
+      name: "Affordable Prices",
+      description:
+        "We don't want you worrying about high monthly costs. Our prices may be low, but we offer the best coverage possible.",
+    },
+    {
+      image: "images/icon-people-first.svg",
+      name: "People First",
+      description:
+        "Our plans aren't full of conditions and clauses to prevent payouts. We make sure your are covered when you need it.",
+    },
+  ];
   return (
-    <div>
+    <div className="container">
       <img src="images/image-intro-desktop.jpg" alt="Family image" />
-      <div className="bg-dark-violet">
+      <div className="bg-dark-violet relative">
         <img
           src="images/bg-pattern-intro-left-mobile.svg"
           alt="Image pattern"
@@ -16,9 +36,34 @@ const MainPage = () => {
             expertise and technology to help you find the plan that's right for
             you. Ensure you and your love ones are protect.
           </p>
-          <button type="button" className="border p-1.5 px-6">
+          <button type="button" className="border p-1.5 px-6 mb-13">
             VIEW PLANS
           </button>
+          <img
+            src="images/bg-pattern-intro-right-mobile.svg"
+            alt="Image pattern"
+            className="absolute right-0 top-77"
+          />
+        </div>
+      </div>
+
+      <div className="flex flex-col items-center mt-37">
+        <div className="w-35 h-[1px] bg-dark-grayish-violet mb-6"></div>
+        <h2 className="text-[2.7rem] font-dmSerif mb-13">We're different</h2>
+        <div className="flex flex-col gap-11 p-6.5 mb-20">
+          {advantages.map((advantage) => (
+            <div key={advantage.name} className="flex flex-col items-center">
+              <img
+                src={advantage.image}
+                alt={advantage.name}
+                className="mb-9"
+              />
+              <h3 className="font-dmSerif text-3xl mb-4">{advantage.name}</h3>
+              <p className="text-center text-dark-grayish-violet">
+                {advantage.description}
+              </p>
+            </div>
+          ))}
         </div>
       </div>
     </div>
